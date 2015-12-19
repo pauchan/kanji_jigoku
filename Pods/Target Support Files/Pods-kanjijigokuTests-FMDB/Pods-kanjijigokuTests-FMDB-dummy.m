3881408240a843a403b4ec8e8cdce1a6402b5c9d
Pawel Rusin <rusin.paw@gmail.com>
2015-12-19 21:18:04 +0900

    added chameleon, cleaned up pods

--- Pods/Target Support Files/Pods-kanjijigokuTests-FMDB/Pods-kanjijigokuTests-FMDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_kanjijigokuTests_FMDB : NSObject
-@end
-@implementation PodsDummy_Pods_kanjijigokuTests_FMDB
-@end
---
+++ Pods/Target Support Files/Pods-kanjijigokuTests/Pods-kanjijigokuTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_kanjijigokuTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_kanjijigokuTestsVersionString[];
+
